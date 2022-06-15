e9482af4be357e3162ff37f101b824cf631bd3e4
TheAesirDev <boludedaniel@gmail.com>
2022-06-15 09:30:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { Routes,Route } from 'react-router-dom';
+import {Homepage,Dashboard,Signup,Sign_In}from './Pages/index'
+function App() {
+  return (
+    <div className="App">
+     <Routes>
+      <Route exact path = "/" element={<Homepage/>}/>
+      <Route exact path = "/Dashboard" element={<Dashboard/>}/>
+      <Route exact path = "/Sign up" element={<Signup/>}/>
+      <Route exact path = "/Sign in" element={<Sign_In/>}/>
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/index.js
@@ -0,0 +1,4 @@
+export {default as Homepage} from './Homepage/Homepage'
+export {default as Dashboard} from './Dashboard/Dashboard'
+export {default as Signup} from './Signup/Signup'
+export {default as Sign_In} from './Sign in/Sign_In'
