9da45e373406c598aff731875be2d8508cbd2366
TheAesirDev <boludedaniel@gmail.com>
2022-06-15 14:41:02 +0100

    commit

--- src/Reusables/Footer/Footer.jsx
+++ src/Reusables/Footer/Footer.jsx
@@ -20,6 +20,52 @@ const Footer = () => {
 
     </div>
   </div>
+  <div className='urcrypto_footer_top_text'>
+  <div className='urcrypto_footer_text'>
+    <h3>Company</h3>
+    <p>About Us</p>
+    <p>Blog</p>
+    <p>Careers</p>
+    <p>Student</p>
+    <p>Security</p>
+    <p>Trust and Safety</p>
+    <p>Newsroom</p>
+    <p>Videos</p>
+  </div>
+  <div className='urcrypto_footer_text'>
+    <h3>Learn</h3>
+    <p> What's Trending</p>
+    <p>Product News</p>
+    <p>Events</p>
+    <p>Student</p>
+    <p>University</p>
+    <p>Market Updates</p>
+  </div>
+  <div className='urcrypto_footer_text'>
+    <h3>Products</h3>
+    <p>Stock & Fund</p>
+    <p>Cash Card</p>
+    <p>Crypto</p>
+    <p>Options</p>
+    <p>Gold</p>
+    <p>Learn Snacks</p>
+  </div>
+  <div className='urcrypto_footer_text'>
+    <h3>Support</h3>
+    <p>Support Center</p>
+    <p>Contact Us</p>
+    <p>System Status</p>
+    <p>Areas of Availability</p>
+  </div>
+  <div className='urcrypto_footer_text'>
+    <h3>Resources</h3>
+    <p>Prices</p>
+    <p>Site</p>
+    <p>Tax</p>
+    <p>Support</p>
+  </div>
+  </div>
+  
 </div>
 <div className='urcrypto_footer_bottom'>
 <p>&copy; Devloped  by Bolude Daniel, All Rights Reserved</p>
